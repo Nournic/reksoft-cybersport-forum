d47a435041198a27762b8bb18daad6bd8d9b152c
Kirill <ushatikovv.kirill@gmail.com>
2026-07-10 18:40:45 +0300

    fullname generation in PlayerMapper

--- src/main/java/com/reksoft/exporter/repository/mapper/PlayerMapper.java
+++ src/main/java/com/reksoft/exporter/repository/mapper/PlayerMapper.java
@@ -8,8 +8,21 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface PlayerMapper {
     @Mapping(target = "combinedName", source = "combinedName")
+    @Mapping(target = "surname", source = "surname")
+    @Mapping(target = "name", source = "name")
     @Mapping(target = "nickname", source = "nickName")
     @Mapping(target = "country", source = "country")
+    @Mapping(target = "fullName", expression = "java(buildFullname(dto.getNickName(), dto.getName(), dto.getSurname()))")
     Player map(PlayerViewDto dto);
+
+    default String buildFullname(String nickname, String name, String surname){
+        StringBuilder fullName = new StringBuilder();
+        fullName.append(surname);
+        fullName.append(" '");
+        fullName.append(nickname);
+        fullName.append("' ");
+        fullName.append(name);
+        return fullName.toString();
+    }
 
 }
--- src/main/java/com/reksoft/exporter/service/PlayerCsvReportService.java
+++ src/main/java/com/reksoft/exporter/service/PlayerCsvReportService.java
@@ -9,42 +9,11 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
 public class PlayerCsvReportService implements ReportService{
 
     private final PlayerService playerService;
-    private static final Map<Integer, String> COUNTRY_MAP = Map.ofEntries(
-            Map.entry(0, "Неизвестно"),
-            Map.entry(1, "Болгария"),
-            Map.entry(2, "Канада"),
-            Map.entry(3, "Китай"),
-            Map.entry(4, "Дания"),
-            Map.entry(5, "Эстония"),
-            Map.entry(6, "Финляндия"),
-            Map.entry(7, "Франция"),
-            Map.entry(8, "Германия"),
-            Map.entry(9, "Греция"),
-            Map.entry(10, "Израиль"),
-            Map.entry(11, "Иордания"),
-            Map.entry(12, "Ливан"),
-            Map.entry(13, "Северная Македония"),
-            Map.entry(14, "Малайзия"),
-            Map.entry(15, "Молдова"),
-            Map.entry(16, "Норвегия"),
-            Map.entry(17, "Пакистан"),
-            Map.entry(18, "Филиппины"),
-            Map.entry(19, "Польша"),
-            Map.entry(20, "Россия"),
-            Map.entry(21, "Сингапур"),
-            Map.entry(22, "Словакия"),
-            Map.entry(23, "Швеция"),
-            Map.entry(24, "Таиланд"),
-            Map.entry(25, "США"),
-            Map.entry(26, "Словакия")
-    );
 
     public File generateReport(String filePath) throws IOException {
         List<Player> players = playerService.getPlayers();
@@ -55,22 +24,13 @@ public class PlayerCsvReportService implements ReportService{
             writer.writeNext(header);
 
             for (Player player : players) {
-                String combinedName = player.getCombinedName();
-                String nickName = player.getNickname();
-                String[] names = combinedName.split(" ", 2);
-                StringBuilder fullName = new StringBuilder();
-                fullName.append(names[0]);
-                fullName.append(" '");
-                fullName.append(nickName);
-                fullName.append("' ");
-                fullName.append(names[1]);
                 String[] line = {
                         String.valueOf(player.getId()),
-                        combinedName,
-                        nickName,
+                        player.getCombinedName(),
+                        player.getNickname(),
                         player.getTeamName(),
-                        player.getCountry() != null ? COUNTRY_MAP.getOrDefault(player.getCountry(), "") : "",
-                        fullName.toString()
+                        player.getCountry() != null ?  player.getCountry() : "",
+                        player.getFullName()
                 };
                 writer.writeNext(line);
             }
